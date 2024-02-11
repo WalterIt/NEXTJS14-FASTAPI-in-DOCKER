6885421f01c9167d5c76f12e54d47c5b4dd6f6fa
WalterIt <wshiruba@gmail.com>
2024-02-11 16:56:10 -0300

    2 - FRONTEND: Basic Set up.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-3xl lg:flex">
-        <h1>Welcome to Docker Nextjs 14!</h1>
+        <h1>Welcome to Docker Nextjs 14! Now my App is running from Docker Container! </h1>
       </div>
     </main>
   );
